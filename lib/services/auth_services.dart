28be4c647b314b017c66d8dda9177d2d7cef6f7b
Kartik Bharti <cartikofficial@gmail.com>
2023-04-27 09:48:36 +0530

    updated

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:groupie/widgets/widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:groupie/services/database_service.dart';
@@ -41,6 +43,34 @@ class Authservices {
   }
 
   // Google Signin
+  Future signinwithgoogle() async {
+    try {
+      final GoogleSignInAccount? googleuser = await GoogleSignIn().signIn();
+      if (googleuser != null) {
+        GoogleSignInAuthentication googleauth = await googleuser.authentication;
+        final OAuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleauth.accessToken,
+          idToken: googleauth.idToken,
+        );
+        // UserCredential userCredential = await auth.signInWithCredential(
+        //   credential,
+        // );
+        // We can also write code in this way
+        // UserCredential userCredential = await auth.signInWithCredential(
+        //   GoogleAuthProvider.credential(
+        //     accessToken: googleauth.accessToken,
+        //     idToken: googleauth.idToken,
+        //   ),
+        // );
+        return true;
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+    return null;
+  }
 
   // Sign-out
   Future signout(context) async {
